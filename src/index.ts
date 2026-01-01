2c37a2f0bf0f0f61cf71d5cdc430bfb23af92f7e
nick-hio <209283586+nick-hio@users.noreply.github.com>
2026-01-01 16:13:48 -0400

    Added MIT license.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import fp from "fastify-plugin";
 import { isValidElement } from "react";
 import { renderToString } from "react-dom/server";
 
-export const fastifyJsx = fp((fastify) => {
+export const fastifyJsx = fp((fastify, _opts, done) => {
     fastify.addHook('preSerialization', async (_req, res, payload) => {
         try {
             if (payload && typeof payload === 'object' && isValidElement(payload)) {
@@ -11,17 +11,19 @@ export const fastifyJsx = fp((fastify) => {
             }
             return payload;
         } catch (e) {
-            fastify.log.error(`[fastify-jsx] preSerialization Error: ${JSON.stringify(e, null, 2)}`)
+            fastify.log.error(`[fastify-jsx] preSerialization Error: ${JSON.stringify(e, null, 2)}`);
             return payload;
         }
-    })
+    });
 
     fastify.addHook('onSend', async (_req, _res, payload) => {
         if (typeof payload === 'string' && payload.length >= 4 && payload.startsWith('"<') && payload.endsWith('>"')) {
             return payload.slice(1, -1).replaceAll('\\"', '"'); // Unescaping quotes from the Fastify's serialization
         }
         return payload;
-    })
+    });
+
+    done();
 });
 
 export default fastifyJsx;
